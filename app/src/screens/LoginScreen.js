9aa99549cc68a4d5f605091c304897e2c120158a
ViniciusFrancaCroa46 <viniciusfrancacoviello@gmail.com>
2025-06-19 00:15:08 -0300

    correção tela login

---
+++ app/src/screens/LoginScreen.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { operadorService } from '../services/api';
+import { useRouter } from 'expo-router';
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      const { data } = await operadorService.loginOp({ email, senha });
+
+      if (data === 'Login realizado com sucesso!') {
+        router.push('./home');
+      } else {
+        Alert.alert('Falha no login', 'Email ou senha incorretos');
+      }
+
+    } catch (error) {
+      console.error(error);
+      Alert.alert('Erro', 'Não foi possível conectar ao servidor');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Login</Text>
+      <TextInput
+        placeholder="Email"
+        style={styles.input}
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Senha"
+        style={styles.input}
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry
+      />
+      <Button title="Entrar" onPress={handleLogin} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  titulo: { fontSize: 24, marginBottom: 20, textAlign: 'center' },
+  input: {
+    borderWidth: 1, borderColor: '#ccc', padding: 10, marginBottom: 15, borderRadius: 5
+  }
+});
